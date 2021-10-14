4928670f9740f210121cd3b348dae5443cef4268
JackAttack612 <jetflyer1130@gmail.com>
2021-10-13 20:53:31 -0400

    Update

--- learn.cpp
+++ learn.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    string charName = "Jackson";
+    int charAge = 69;
+    string pronouns = "ze";
+    cout << "There one was a " << pronouns << " named " << charName<< endl;
+    cout << pronouns << " was " << charAge << " years old" << endl;
+    cout << pronouns << " really liked the name " << charName << endl;
+    cout << "but didn't like being " << charAge << "." << endl;
+
+    return 0;
 }
