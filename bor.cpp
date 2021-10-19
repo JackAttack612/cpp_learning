668464b71a20894f1ad19f158d0eeb747035036c
JackAttack612 <jetflyer1130@gmail.com>
2021-10-19 15:32:16 -0400

    Update

---
+++ bor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Bowl of Rice:" << endl;
+    cout << "                     //" << endl;
+    cout << "                    //" << endl;
+    cout << "___________________//________" << endl;
+    cout << "\\                          /" << endl;
+    cout << " \\                        /" << endl;
+    cout << "  \\                      /" << endl;
+    cout << "   \\                    /" << endl;
+    cout << "    \\                  /" << endl;
+    cout << "     \\                /" << endl;
+    cout << "      \\______________/" << endl;
+    cout << "Inspired by: Audrey Ellen Miller" << endl;
+    cout << "Made by: Jackson Edward Tavares" << endl;
+
+    
+
+    return 0;
+}
