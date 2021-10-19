5af49f1091849a10d2e7e9aecbe50ab98f892362
JackAttack612 <jetflyer1130@gmail.com>
2021-10-19 11:16:03 -0400

    Update

--- learn.cpp
+++ learn.cpp
@@ -14,10 +14,10 @@ int main()
     getline(cin, charName);
     cout << "What is your characters age: ";
     cin >> charAge;
-    cout << "Is your Character a boy or girl: ";
+    cin.ignore();
+    cout << "What are your characters pronouns (He or She): ";
     getline(cin, pronoun);
-    cout << endl;
-    cout << "There one was a " << pronoun << " named " << charName<< endl;
+    cout << endl << "There one was a " << pronoun << " named " << charName<< endl;
     cout << pronoun << " was " << charAge << " years old" << endl;
     cout << pronoun << " really liked the name " << charName << endl;
     cout << "but didn't like being " << charAge << "." << endl;
